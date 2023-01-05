43726c407f44efb5fda06f573285cb46af0e7597
540shalini <121996159+540shalini@users.noreply.github.com>
2023-01-05 09:44:02 +0530

    Add files via upload

---
+++ manasvi.py
@@ -0,0 +1,13 @@
+class number:
+    num=55
+    def show(self):
+        if(self.num%2==0):
+            print("num is even")
+            else:
+                print("num is odd")
+manu=number()
+manu=show()
+
+
+    
+
